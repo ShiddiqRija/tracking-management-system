07a3fec52bee46e81888062c33318089c5ddf140
ShiddiqRija <shiddiq100@gmail.com>
2023-04-03 09:49:10 +0700

    change input type text to email

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -46,7 +46,7 @@
                                     {{ csrf_field() }}
                                     <div class="mb-3">
                                         <label for="email">Email</label>
-                                        <input type="text" class="form-control" id="email" name="email" placeholder="Enter email">
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
                                     </div>
                                     <div class="mb-3">
                                         <label for="password">Password</label>
